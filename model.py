bc8786217b033e481376296ee230d65aa9cb05d9
peterkaloyannis <peter.kaloyannis@outlook.com>
2020-06-27 21:37:46 -0400

    kms

--- model.py
+++ model.py
@@ -1,8 +1,9 @@
 import numpy as np
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
 import torch as t
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.utils.data as feeder
 import torch.optim as optim
 import torch.utils.data as data_utils
 
@@ -27,30 +28,43 @@ if __name__ == "__main__":
     if t.cuda.is_available():  # verifies if gpu is available on the current machine
         model = model.cuda()  # transfers the model to gpu memory
 
-    optimizer = optim.Adam(model.parmeters(), lr=0.01)
-    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=0.0005)
+    criterion = nn.BCELoss()
+
+    data = t.Tensor(np.load("data_processed.npy")).cuda()
+    answers = t.Tensor(np.load("data_truth.npy")).cuda()
+    validation = t.Tensor(np.load("data_processed1.npy")[:2]).cuda()
+    valians = t.Tensor(np.load("data_truth1.npy")[:2]).cuda()
+
+    dataset = feeder.TensorDataset(data, answers)
+    loader = feeder.DataLoader(dataset, batch_size=2)
 
     for epoch in range(2):  # loop over the dataset multiple times
 
         running_loss = 0.0
-        for i, data in enumerate(trainloader, 0):
+        for i, data in enumerate(loader, 0):
             # get the inputs; data is a list of [inputs, labels]
-            inputs, outputs = data
-
+            inputs, solutions = data
             # zero the parameter gradients
             optimizer.zero_grad()
 
             # forward + backward + optimize
             outputs = model(inputs)
-            loss = criterion(outputs, outputs)
+            # print(outputs.shape)
+            loss = criterion(outputs, solutions.cuda())
             loss.backward()
             optimizer.step()
 
             # print statistics
             running_loss += loss.item()
-            if i % 2000 == 1999:    # print every 2000 mini-batches
-                print('[%d, %5d] loss: %.3f' %
-                      (epoch + 1, i + 1, running_loss / 2000))
-                running_loss = 0.0
+            # if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+    print('Finished Training')
 
-print('Finished Training')
+    valout = model(validation)
+    plt.imshow(valians[0, 0].cpu())
+    plt.show()
+    plt.imshow(valout[0, 0].cpu().detach().numpy())
+    plt.show()
